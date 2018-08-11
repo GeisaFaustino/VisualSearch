4e74ad87077af55c9c6628f84463d2d137812c61
Geisa Faustino <gefausti@microsoft.com>
2018-08-11 00:38:15 -0300

    Add notebook (tests) about SDK

--- utils.py
+++ utils.py
@@ -1,5 +1,38 @@
 import json
-    
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import matplotlib.image as mpimg
+import requests
+
+
+
 def print_json(obj):
     """Print the object as json"""
     print(json.dumps(obj, sort_keys=True, indent=2, separators=(',', ': ')))
+    
+    
+    
+def rendering_input_image(image_path):
+    image = mpimg.imread(image_path)
+    plt.title('Input image')
+    plt.imshow(image)
+    plt.show()
+    
+
+    
+def rendering_images_grid(m, n, images_data):
+    f, axes = plt.subplots(m, n, figsize=(32,32))
+    for i in range(m):
+        for j in range(n):
+            indice = i * n + j 
+            if indice < len(images_data):
+                img_data = images_data[ indice ]
+                thumbnail_url = img_data[0]
+                host_page_url = img_data[1]
+                data = requests.get( thumbnail_url )
+                data.raise_for_status()
+                image = Image.open( BytesIO( data.content ) )
+                axes[i][j].imshow( image )
+                axes[i][j].axis( "off" )               
---
+++ visualsearch.py
@@ -0,0 +1,77 @@
+import http.client, urllib.parse
+import json
+from azure.cognitiveservices.search.visualsearch import VisualSearchAPI
+from azure.cognitiveservices.search.visualsearch.models import (
+    VisualSearchRequest,
+    CropArea,
+    ImageInfo,
+    Filters,
+    KnowledgeRequest,
+)
+from msrest.authentication import CognitiveServicesCredentials
+
+
+def search( image_path, subscription_key, knowledge_request ):
+    
+    thumbnail_url_and_host_page_url = []
+
+    # Instantiate the client
+    client = VisualSearchAPI( CognitiveServicesCredentials(subscription_key) )
+       
+    # Using the client to search images and parse results
+    with open(image_path, "rb") as image_fd:
+
+        # You need to pass the serialized form of the model
+        knowledge_request = json.dumps( knowledge_request.serialize() )
+
+        print("\r\nSearch visual search request with binary of input image")
+        result = client.images.visual_search( image = image_fd, knowledge_request = knowledge_request )
+
+        if not result:
+            print("No visual search result data.")
+            return
+
+        # Visual Search results
+        if result.image.image_insights_token:
+            print("Uploaded image insights token: {}".format( result.image.image_insights_token ) )
+        else:
+            print("Couldn't find image insights token!")
+
+        if result.tags:
+            all_urls = result.tags[0].actions[2].data.value
+            print( len(all_urls) )
+            thumbnail_url_and_host_page_url = [(url.thumbnail_url, url.host_page_url) for url in all_urls]
+            #for url in all_urls:
+                #thumbnail_url_and_host_page_url.append( (url.thumbnail_url, url.host_page_url) )
+                
+    return  thumbnail_url_and_host_page_url
+
+
+
+def cropped_image( image_path, subscription_key, top ,bottom ,left ,right ):
+    
+    crop_area = CropArea( top = top, bottom = bottom, left = left, right = right )
+    knowledge_request = VisualSearchRequest( image_info = ImageInfo( crop_area = crop_area ) ) 
+    
+    return search( image_path, subscription_key, knowledge_request )
+
+
+
+def binary_image( image_path, subscription_key ):
+    
+    knowledge_request = VisualSearchRequest()
+    
+    return search( image_path, subscription_key, knowledge_request )
+
+
+
+def search_url_with_filter( image_path, subscription_key, image_url ):
+
+    filters = Filters( site="www.bing.com" )
+
+    knowledge_request = VisualSearchRequest(
+        image_info = ImageInfo( url = image_url ),
+        knowledge_request = KnowledgeRequest( filters = filters )
+    )
+
+    return search( image_path, subscription_key, knowledge_request )
